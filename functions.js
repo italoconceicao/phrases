afcad14a1e6fde3dee92a04c2cdc2b97541f2c19
Ítalo Conceição <56076361+italoconceicao@users.noreply.github.com>
2021-11-24 06:04:11 -0300

    Update functions.js
    small improvements:
    
    - primeira frase é exibida.
    - var frases deve virar um objeto
    - mudarFrase() deve ser aleatório

--- functions.js
+++ functions.js
@@ -1,5 +1,12 @@
-var frases = {
-    frase1 = "",
-    frase2 = "".
-    frase3 = ""
-};
+// transformei em array mas provavelmente será um objeto.
+var frases = ["Primeira frase","Segunda frase","Terceira frase"];
+
+
+var fraseExibida = document.getElementById("frase");
+var proximaFrase = document.getElementById("proximo");
+
+proximaFrase.addEventListener("click", mudarFrase);
+
+function mudarFrase(){
+    fraseExibida.innerHTML = frases[0];
+}
