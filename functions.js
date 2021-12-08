753ac0f2bb81b0fc07039503e1041feebb7594be
Ítalo Conceição <56076361+italoconceicao@users.noreply.github.com>
2021-12-08 00:30:55 -0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,13 +1,22 @@
 // transformei em array mas provavelmente será um objeto.
 var frases = [
-"“Os capitalistas chamam 'liberdade' a dos ricos de enriquecer e a dos operários para morrer de fome. Os capitalistas chamam liberdade de imprensa a compra dela pelos ricos, servindo-se da riqueza para fabricar e falsificar a opinião pública.” LENIN",
-"“É verdade que a liberdade é preciosa - tão preciosa que precisa ser racionalizada.” LENIN",
-"“As revoluções são as festas dos oprimidos e explorados.” LENIN"
+
+"“Os capitalistas chamam 'liberdade' a dos ricos de enriquecer e a dos operários para morrer de fome. Os capitalistas chamam liberdade de imprensa a compra dela pelos ricos, servindo-se da riqueza para fabricar e falsificar a opinião pública.”" + "<br>" + "LENIN",
+"“É verdade que a liberdade é preciosa - tão preciosa que precisa ser racionalizada.”" + "<br>" + "LENIN",
+"“As revoluções são as festas dos oprimidos e explorados.”" + "<br>" + "LENIN",
+"“A propriedade do Estado não é a de 'todo povo', mas que na medida em que desaparecem os privilégios e as distinções sociais e em que, em consequência, o Estado perde sua razão de ser. Dito de outra maneira: a propriedade do Estado se faz socialista a medida que deixa de ser propriedade do Estado.”" + "<br>" + "TROTSKY",
+"“Num país em que o único empregador é o Estado, oposição significa morte lenta por inanição. O velho princípio 'quem não trabalha não come' foi substituído por outro: quem não obedece não come.”" + "<br>" + "TROTSKY",
+"“Ela virá, a revolução conquistará a todos o direito não somente ao pão mas, também, à poesia.”" + "<br>" + "TROTSKY",
+"“A morte de uma pessoa é uma tragédia; a de milhões, uma estatística.”" + "<br>" + "STALIN",
+
 ];
 
 
 var fraseExibida = document.getElementById("frase");
 var proximaFrase = document.getElementById("proximafrase");
+
+const fraseAleatoria = frases[Math.floor(Math.random() * frases.length)];
+    fraseExibida.innerHTML = fraseAleatoria;
 
 proximaFrase.addEventListener("click", mudarFrase);
 
