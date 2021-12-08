2bdae98981ef1801a768f31c9514f959546f027c
Ítalo Conceição <56076361+italoconceicao@users.noreply.github.com>
2021-12-07 22:51:03 -0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,12 +1,17 @@
 // transformei em array mas provavelmente será um objeto.
-var frases = ["Primeira frase","Segunda frase","Terceira frase"];
+var frases = [
+"“Os capitalistas chamam 'liberdade' a dos ricos de enriquecer e a dos operários para morrer de fome. Os capitalistas chamam liberdade de imprensa a compra dela pelos ricos, servindo-se da riqueza para fabricar e falsificar a opinião pública.” LENIN",
+"“É verdade que a liberdade é preciosa - tão preciosa que precisa ser racionalizada.” LENIN",
+"“As revoluções são as festas dos oprimidos e explorados.” LENIN"
+];
 
 
 var fraseExibida = document.getElementById("frase");
-var proximaFrase = document.getElementById("proximo");
+var proximaFrase = document.getElementById("proximafrase");
 
 proximaFrase.addEventListener("click", mudarFrase);
 
 function mudarFrase(){
-    fraseExibida.innerHTML = frases[0];
+    const fraseAleatoria = frases[Math.floor(Math.random() * frases.length)];
+    fraseExibida.innerHTML = fraseAleatoria;
 }
