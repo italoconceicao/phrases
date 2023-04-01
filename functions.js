69e1cab86c981a9b025db1ae31484f8a09b8ee65
Italo Conceição <italooliveiradaconceicao@gmail.com>
2023-04-01 14:37:54 -0300

    Bug de Caractere Resolvido

--- functions.js
+++ functions.js
@@ -1,73 +1,73 @@
 var frases = [
 
-// 
-"“Os capitalistas chamam 'liberdade' a dos ricos de enriquecer e a dos operários para morrer de fome. Os capitalistas chamam liberdade de imprensa a compra dela pelos ricos, servindo-se da riqueza para fabricar e falsificar a opinião pública.”" + "<br><br>" + "Vladimir LENIN",
-"“É verdade que a liberdade é preciosa - tão preciosa que precisa ser racionalizada.”" + "<br><br>" + "Vladimir LENIN",
-"“As revoluções são as festas dos oprimidos e explorados.”" + "<br><br>" + "Vladimir LENIN",
-//
-"“É melhor sacrificar tudo do que viver na escravidão!”" + "<br><br>" + "Ho Chi Minh",
-"“Pessoas que saem da prisão podem construir o país. O infortúnio é um teste à fidelidade. E aqueles que protestam contra injustiça são pessoas de verdadeiro mérito. Quando as portas da prisão são abertas, o real dragão voa.”" + "<br><br>" + "Ho Chi Minh",
-//
-"“A propriedade do Estado não é a de 'todo povo', mas que na medida em que desaparecem os privilégios e as distinções sociais e em que, em consequência, o Estado perde sua razão de ser. Dito de outra maneira: a propriedade do Estado se faz socialista a medida que deixa de ser propriedade do Estado.”" + "<br><br>" + "Leon TROTSKY",
-"“Num país em que o único empregador é o Estado, oposição significa morte lenta por inanição. O velho princípio 'quem não trabalha não come' foi substituído por outro: quem não obedece não come.”" + "<br><br>" + "Leon TROTSKY",
-"“Ela virá, a revolução conquistará a todos o direito não somente ao pão mas, também, à poesia.”" + "<br><br>" + "Leon TROTSKY",
-//
-"“Seja resoluto, não tenha medo de sacrifícios e derrube todas as dificuldades para a vitória.”" + "<br><br>" + "Mao Zedong",
-"“A revolução não é o convite para um jantar, a composição de uma obra literária, a pintura de um quadro ou a confecção de um bordado, ela não pode ser assim tão refinada, calma e delicada, tão branda, tão afável e cortês, comedida e generosa. A revolução é um insurreição, é um ato de violência pelo qual uma classe derruba a outra.”" + "<br><br>" + "Mao Zedong",
-//    
-"“A morte de uma pessoa é uma tragédia; a de milhões, uma estatística.”" + "<br><br>" + "Joseph STALIN",
-"“Líderes vão e vem, mas o povo permanece. Apenas o povo é imortal.”" + "<br><br>" + "Joseph STALIN",
-"“O povo deve ser educado com o mesmo cuidado e ternura com que um jardineiro cultiva uma árvore frutífera de estimação.”" + "<br><br>" + "Joseph STALIN",
-"“O marxismo não é apenas a teoria do socialismo, é uma concepção integral do mundo, um sistema filosófico no qual decorre, logicamente, o socialismo proletário de Marx. Esse sistema filosófico se chama materialismo dialético.”" + "<br><br>" + "Joseph STALIN",
-"“A revolução vitoriosa num país tem por tarefa desenvolver e sustentar a revolução nos outros países.”" + "<br><br>" + "Joseph STALIN",
-"“Teríamos acabado sozinhos se governássemos apenas pelo medo. A classe operária jamais se submeteria a um governo que pretendesse impor-se pelo medo.”" + "<br><br>" + "Joseph STALIN",
-"“O senhor parte da presunção de que todos os homens são bons. Eu, entretanto, não posso esquecer que há muitos homens perversos. Não creio na bondade da burguesia.”" + "<br><br>" + "Joseph STALIN",
-//
-"“Sonha e serás livre de espírito... luta e serás livre na vida.”" + "<br><br>" + "Ernesto CHE GUEVARA", 
-"“Se você é capaz de tremer de indignação a cada vez que se comete uma injustiça no mundo, então somos companheiros.”" + "<br><br>" + "Ernesto CHE GUEVARA",
-"“O conhecimento nos faz responsáveis.”" + "<br><br>" + "Ernesto CHE GUEVARA",
-"“Acima de tudo procurem sentir no mais profundo de vocês qualquer injustiça cometida contra qualquer pessoa em qualquer parte do mundo. É a mais bela qualidade de um revolucionário.”" + "<br><br>" + "Ernesto CHE GUEVARA",
-"“Os grandes só parecem grandes porque estamos ajoelhados.”" + "<br><br>" + "Ernesto CHE GUEVARA",
-"“O importante não é justificar o erro, mas impedir que ele se repita.”" + "<br><br>" + "Ernesto CHE GUEVARA",
-"“A farda modela o corpo e atrofia a mente.”" + "<br><br>" + "Ernesto CHE GUEVARA",
-"“Deixe-me dizer-lhe, correndo o risco de parecer ridículo, que o verdadeiro revolucionário é guiado por grandes sentimentos de amor.”" + "<br><br>" + "Ernesto CHE GUEVARA",
-"“Não nego a necessidade objetiva do estímulo material, mas sou contrário a utilizá-lo como alavanca impulsora fundamental. Porque então ela termina por impor sua própria força às relações entre os homens.”" + "<br><br>" + "Ernesto CHE GUEVARA",
-"“A reforma agrária radical é a única que pode dar a terra ao camponês.”" + "<br><br>" + "Ernesto CHE GUEVARA",
-"“Só existe um sentimento maior que o amor à liberdade: o ódio ao que a tira de você.”" + "<br><br>" + "Ernesto CHE GUEVARA",
-"“É preciso lutar todos os dias para que esse amor à humanidade viva se transforme em fatos concretos, em atos que sirvam de exemplo de mobilização.”" + "<br><br>" + "Ernesto CHE GUEVARA",
-//
-"“Camaradas, não há verdadeira revolução social sem a libertação das mulheres. Que meus olhos nunca vejam e meus pés nunca me levem a uma sociedade onde metade das pessoas são mantidas em silêncio. Eu ouço o rugido do silêncio das mulheres. Sinto o estrondo da tempestade deles e sinto a fúria da revolta deles.”" + "<br><br>" + "Thomas SANKARA",
-"“O imperialismo é um sistema de exploração que ocorre não só na forma brutal daqueles que vêm com armas para conquistar território. O imperialismo frequentemente ocorre em formas mais sutis, um empréstimo, ajuda alimentar, chantagem. Estamos lutando contra esse sistema que permite que um punhado de homens na Terra governe toda a humanidade.”" + "<br><br>" + "Thomas SANKARA",
-"“Nossa revolução não é um torneio de falar em público. Nossa revolução não é uma batalha de frases finas. Nossa revolução não é apenas para divulgar slogans que não passam de sinais usados por manipuladores que tentam usá-los como palavras de ordem, como palavras de código, como uma película para sua própria exibição. Nossa revolução é, e deve continuar sendo, o esforço coletivo dos revolucionários para transformar a realidade, para melhorar a situação concreta das massas de nosso país.”" + "<br><br>" + "Thomas SANKARA",
-//
-"“A religião nasce das concepções restritas do homem.”" + "<br><br>" + "Friedrich ENGELS",
-"“Os que no regime burguês trabalham não lucram e os que lucram não trabalham.”" + "<br><br>" + "Friedrich ENGELS",
-"“Não pode ser livre um povo que oprime outros povos.”" + "<br><br>" + "Friedrich ENGELS",
-//
-"“A propaganda não pode ser abandonada nem por um minuto, porque é a alma da nossa luta””" + "<br><br>" + "FIDEL Castro",
-"“Esta é a Revolução socialista e democrática dos humildes, com os humildes e para os humildes””" + "<br><br>" + "FIDEL Castro",
-"“Revolução é mudar tudo o que deve ser mudado””" + "<br><br>" + "FIDEL Castro",
-"“Uma revolução não é um mar de rosas. É uma luta de morte entre o futuro e o passado.”" + "<br><br>" + "FIDEL Castro",
-"“Não nos enganemos achando que adiante tudo será fácil; talvez tudo seja mais difícil.”" + "<br><br>" + "FIDEL Castro",
-//
-"“Se você não for atento e cuidadoso, os jornais o farão odiar as pessoas que estão sendo oprimidas e amar as pessoas que são opressoras.”" + "<br><br>" + "MALCOLM X",
-"“Não se pode ter capitalismo sem racismo.”" + "<br><br>" + "MALCOLM X",
-//
-"“O desenvolvimento da sociedade humana, do capitalismo ao socialismo, é uma lei imutável da história e somente quando segue-se o caminho do socialismo é possível fazer realidade o ideal da humanidade de viver de modo independente e em paz.”" + "<br><br>" + "Kim Il Sung",
-"“O socialismo é a maior conquista da humanidade e o ideal que lhe oferece vigorosa vitalidade e esperança.”" + "<br><br>" + "Kim Il Sung",
-"“A revolução social não se realiza com facilidade, como se em plena luz do dia seguisse por um caminho seguro e amplo, nem como se navegasse tranquilamente e em pleno vapor, sem dificuldade.”" + "<br><br>" + "Kim Il Sung",
-"“Não apenas derrubar o velho sistema social e construir um novo é a revolução, mas também mudar o obsoleto para o novo nas esferas ideológica, técnica e cultural.”" + "<br><br>" + "Kim Il Sung",
-"“Somente quando se unem e cooperam, as massas populares podem ser entidades mais poderosas capazes de transformar e renovar o mundo.”" + "<br><br>" + "Kim Il Sung",
-//
-"“Quem não se movimenta, não sente as correntes que o prendem.”" + "<br><br>" + "Rosa Luxemburgo",
-"“É preciso auto-disciplina interior, maturidade intelectual, seriedade moral, senso de dignidade e de responsabilidade, todo um renascimento interior do proletário. Com homens preguiçosos, levianos, egoístas, irrefletidos e indiferentes não se pode realizar o socialismo.”" + "<br><br>" + "Rosa Luxemburgo",
-"“Não estamos perdidos. Ao contrário, venceremos se não tivermos desaprendido a aprender.”" + "<br><br>" + "Rosa Luxemburgo",
-//    
-"“No começo pensei que estivesse lutando para salvar seringueiras, depois pensei que estava lutando para salvar a Floresta Amazônica. Agora, percebo que estou lutando pela humanidade.”" + "<br><br>" + "Chico Mendes",
-⁠"“Ecologia sem luta de classes é jardinagem.”" + "<br><br>" + "Chico Mendes",
-"“Se descesse um enviado dos céus e me garantisse que minha morte iria fortalecer nossa luta até que valeria a pena. Mas a experiência nos ensina o contrário. Então eu quero viver. Ato público e enterro numeroso não salvarão a Amazônia. Quero Viver.”" + "<br><br>" + "Chico Mendes",
-//
-    
+    // 
+    "“Os capitalistas chamam 'liberdade' a dos ricos de enriquecer e a dos operários para morrer de fome. Os capitalistas chamam liberdade de imprensa a compra dela pelos ricos, servindo-se da riqueza para fabricar e falsificar a opinião pública.”" + "<br><br>" + "Vladimir LENIN",
+    "“É verdade que a liberdade é preciosa - tão preciosa que precisa ser racionalizada.”" + "<br><br>" + "Vladimir LENIN",
+    "“As revoluções são as festas dos oprimidos e explorados.”" + "<br><br>" + "Vladimir LENIN",
+    //
+    "“É melhor sacrificar tudo do que viver na escravidão!”" + "<br><br>" + "Ho Chi Minh",
+    "“Pessoas que saem da prisão podem construir o país. O infortúnio é um teste à fidelidade. E aqueles que protestam contra injustiça são pessoas de verdadeiro mérito. Quando as portas da prisão são abertas, o real dragão voa.”" + "<br><br>" + "Ho Chi Minh",
+    //
+    "“A propriedade do Estado não é a de 'todo povo', mas que na medida em que desaparecem os privilégios e as distinções sociais e em que, em consequência, o Estado perde sua razão de ser. Dito de outra maneira: a propriedade do Estado se faz socialista a medida que deixa de ser propriedade do Estado.”" + "<br><br>" + "Leon TROTSKY",
+    "“Num país em que o único empregador é o Estado, oposição significa morte lenta por inanição. O velho princípio 'quem não trabalha não come' foi substituído por outro: quem não obedece não come.”" + "<br><br>" + "Leon TROTSKY",
+    "“Ela virá, a revolução conquistará a todos o direito não somente ao pão mas, também, à poesia.”" + "<br><br>" + "Leon TROTSKY",
+    //
+    "“Seja resoluto, não tenha medo de sacrifícios e derrube todas as dificuldades para a vitória.”" + "<br><br>" + "Mao Zedong",
+    "“A revolução não é o convite para um jantar, a composição de uma obra literária, a pintura de um quadro ou a confecção de um bordado, ela não pode ser assim tão refinada, calma e delicada, tão branda, tão afável e cortês, comedida e generosa. A revolução é um insurreição, é um ato de violência pelo qual uma classe derruba a outra.”" + "<br><br>" + "Mao Zedong",
+    //    
+    "“A morte de uma pessoa é uma tragédia; a de milhões, uma estatística.”" + "<br><br>" + "Joseph STALIN",
+    "“Líderes vão e vem, mas o povo permanece. Apenas o povo é imortal.”" + "<br><br>" + "Joseph STALIN",
+    "“O povo deve ser educado com o mesmo cuidado e ternura com que um jardineiro cultiva uma árvore frutífera de estimação.”" + "<br><br>" + "Joseph STALIN",
+    "“O marxismo não é apenas a teoria do socialismo, é uma concepção integral do mundo, um sistema filosófico no qual decorre, logicamente, o socialismo proletário de Marx. Esse sistema filosófico se chama materialismo dialético.”" + "<br><br>" + "Joseph STALIN",
+    "“A revolução vitoriosa num país tem por tarefa desenvolver e sustentar a revolução nos outros países.”" + "<br><br>" + "Joseph STALIN",
+    "“Teríamos acabado sozinhos se governássemos apenas pelo medo. A classe operária jamais se submeteria a um governo que pretendesse impor-se pelo medo.”" + "<br><br>" + "Joseph STALIN",
+    "“O senhor parte da presunção de que todos os homens são bons. Eu, entretanto, não posso esquecer que há muitos homens perversos. Não creio na bondade da burguesia.”" + "<br><br>" + "Joseph STALIN",
+    //
+    "“Sonha e serás livre de espírito... luta e serás livre na vida.”" + "<br><br>" + "Ernesto CHE GUEVARA",
+    "“Se você é capaz de tremer de indignação a cada vez que se comete uma injustiça no mundo, então somos companheiros.”" + "<br><br>" + "Ernesto CHE GUEVARA",
+    "“O conhecimento nos faz responsáveis.”" + "<br><br>" + "Ernesto CHE GUEVARA",
+    "“Acima de tudo procurem sentir no mais profundo de vocês qualquer injustiça cometida contra qualquer pessoa em qualquer parte do mundo. É a mais bela qualidade de um revolucionário.”" + "<br><br>" + "Ernesto CHE GUEVARA",
+    "“Os grandes só parecem grandes porque estamos ajoelhados.”" + "<br><br>" + "Ernesto CHE GUEVARA",
+    "“O importante não é justificar o erro, mas impedir que ele se repita.”" + "<br><br>" + "Ernesto CHE GUEVARA",
+    "“A farda modela o corpo e atrofia a mente.”" + "<br><br>" + "Ernesto CHE GUEVARA",
+    "“Deixe-me dizer-lhe, correndo o risco de parecer ridículo, que o verdadeiro revolucionário é guiado por grandes sentimentos de amor.”" + "<br><br>" + "Ernesto CHE GUEVARA",
+    "“Não nego a necessidade objetiva do estímulo material, mas sou contrário a utilizá-lo como alavanca impulsora fundamental. Porque então ela termina por impor sua própria força às relações entre os homens.”" + "<br><br>" + "Ernesto CHE GUEVARA",
+    "“A reforma agrária radical é a única que pode dar a terra ao camponês.”" + "<br><br>" + "Ernesto CHE GUEVARA",
+    "“Só existe um sentimento maior que o amor à liberdade: o ódio ao que a tira de você.”" + "<br><br>" + "Ernesto CHE GUEVARA",
+    "“É preciso lutar todos os dias para que esse amor à humanidade viva se transforme em fatos concretos, em atos que sirvam de exemplo de mobilização.”" + "<br><br>" + "Ernesto CHE GUEVARA",
+    //
+    "“Camaradas, não há verdadeira revolução social sem a libertação das mulheres. Que meus olhos nunca vejam e meus pés nunca me levem a uma sociedade onde metade das pessoas são mantidas em silêncio. Eu ouço o rugido do silêncio das mulheres. Sinto o estrondo da tempestade deles e sinto a fúria da revolta deles.”" + "<br><br>" + "Thomas SANKARA",
+    "“O imperialismo é um sistema de exploração que ocorre não só na forma brutal daqueles que vêm com armas para conquistar território. O imperialismo frequentemente ocorre em formas mais sutis, um empréstimo, ajuda alimentar, chantagem. Estamos lutando contra esse sistema que permite que um punhado de homens na Terra governe toda a humanidade.”" + "<br><br>" + "Thomas SANKARA",
+    "“Nossa revolução não é um torneio de falar em público. Nossa revolução não é uma batalha de frases finas. Nossa revolução não é apenas para divulgar slogans que não passam de sinais usados por manipuladores que tentam usá-los como palavras de ordem, como palavras de código, como uma película para sua própria exibição. Nossa revolução é, e deve continuar sendo, o esforço coletivo dos revolucionários para transformar a realidade, para melhorar a situação concreta das massas de nosso país.”" + "<br><br>" + "Thomas SANKARA",
+    //
+    "“A religião nasce das concepções restritas do homem.”" + "<br><br>" + "Friedrich ENGELS",
+    "“Os que no regime burguês trabalham não lucram e os que lucram não trabalham.”" + "<br><br>" + "Friedrich ENGELS",
+    "“Não pode ser livre um povo que oprime outros povos.”" + "<br><br>" + "Friedrich ENGELS",
+    //
+    "“A propaganda não pode ser abandonada nem por um minuto, porque é a alma da nossa luta””" + "<br><br>" + "FIDEL Castro",
+    "“Esta é a Revolução socialista e democrática dos humildes, com os humildes e para os humildes””" + "<br><br>" + "FIDEL Castro",
+    "“Revolução é mudar tudo o que deve ser mudado””" + "<br><br>" + "FIDEL Castro",
+    "“Uma revolução não é um mar de rosas. É uma luta de morte entre o futuro e o passado.”" + "<br><br>" + "FIDEL Castro",
+    "“Não nos enganemos achando que adiante tudo será fácil; talvez tudo seja mais difícil.”" + "<br><br>" + "FIDEL Castro",
+    //
+    "“Se você não for atento e cuidadoso, os jornais o farão odiar as pessoas que estão sendo oprimidas e amar as pessoas que são opressoras.”" + "<br><br>" + "MALCOLM X",
+    "“Não se pode ter capitalismo sem racismo.”" + "<br><br>" + "MALCOLM X",
+    //
+    "“O desenvolvimento da sociedade humana, do capitalismo ao socialismo, é uma lei imutável da história e somente quando segue-se o caminho do socialismo é possível fazer realidade o ideal da humanidade de viver de modo independente e em paz.”" + "<br><br>" + "Kim Il Sung",
+    "“O socialismo é a maior conquista da humanidade e o ideal que lhe oferece vigorosa vitalidade e esperança.”" + "<br><br>" + "Kim Il Sung",
+    "“A revolução social não se realiza com facilidade, como se em plena luz do dia seguisse por um caminho seguro e amplo, nem como se navegasse tranquilamente e em pleno vapor, sem dificuldade.”" + "<br><br>" + "Kim Il Sung",
+    "“Não apenas derrubar o velho sistema social e construir um novo é a revolução, mas também mudar o obsoleto para o novo nas esferas ideológica, técnica e cultural.”" + "<br><br>" + "Kim Il Sung",
+    "“Somente quando se unem e cooperam, as massas populares podem ser entidades mais poderosas capazes de transformar e renovar o mundo.”" + "<br><br>" + "Kim Il Sung",
+    //
+    "“Quem não se movimenta, não sente as correntes que o prendem.”" + "<br><br>" + "Rosa Luxemburgo",
+    "“É preciso auto-disciplina interior, maturidade intelectual, seriedade moral, senso de dignidade e de responsabilidade, todo um renascimento interior do proletário. Com homens preguiçosos, levianos, egoístas, irrefletidos e indiferentes não se pode realizar o socialismo.”" + "<br><br>" + "Rosa Luxemburgo",
+    "“Não estamos perdidos. Ao contrário, venceremos se não tivermos desaprendido a aprender.”" + "<br><br>" + "Rosa Luxemburgo",
+    //    
+    "“No começo pensei que estivesse lutando para salvar seringueiras, depois pensei que estava lutando para salvar a Floresta Amazônica. Agora, percebo que estou lutando pela humanidade.”" + "<br><br>" + "Chico Mendes",
+    "“Ecologia sem luta de classes é jardinagem.”" + "<br><br>" + "Chico Mendes",
+    "“Se descesse um enviado dos céus e me garantisse que minha morte iria fortalecer nossa luta até que valeria a pena. Mas a experiência nos ensina o contrário. Então eu quero viver. Ato público e enterro numeroso não salvarão a Amazônia. Quero Viver.”" + "<br><br>" + "Chico Mendes",
+    //
+
 ];
 
 
@@ -78,7 +78,7 @@ mudarFrase();
 
 proximaFrase.addEventListener("click", mudarFrase);
 
-function mudarFrase(){
+function mudarFrase() {
     const fraseAleatoria = frases[Math.floor(Math.random() * frases.length)];
     fraseExibida.innerHTML = fraseAleatoria;
 }
